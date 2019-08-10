08a6df641578f8d34a2e92c3e17393bf85997e40
koin <kangyueluo777@gmail.com>
2019-08-10 11:38:21 +0800

    Initial commit

---
+++ test/test.dart
@@ -0,0 +1,40 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_app46/main.dart';
+
+void main() {
+  testWidgets('Add and remove a todo', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(TodoList());
+
+    // Enter 'hi' into the TextField.
+    await tester.enterText(find.byType(TextField), 'hi');
+
+    // Tap the add button.
+    await tester.tap(find.byType(FloatingActionButton));
+
+    // Rebuild the widget after the state has changed.
+    await tester.pump();
+
+    // Expect to find the item on screen.
+    expect(find.text('hi'), findsOneWidget);
+
+    // Swipe the item to dismiss it.
+    await tester.drag(find.byType(Dismissible), Offset(500.0, 0.0));
+
+    // Build the widget until the dismiss animation ends.
+    await tester.pumpAndSettle();
+
+    // Ensure that the item is no longer on screen.
+    expect(find.text('hi'), findsNothing);
+
+  });
+}
